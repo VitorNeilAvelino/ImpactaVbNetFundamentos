dcdbeb818dbe168c5ee8959f2f33606a68ddbbe1
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-02-04 15:56:55 +0000

    Adição de método para contar dias úteis entre datas.

---
+++ PlantaoDeDuvidas/Impacta.Infra.Apoio.Testes/CalcularTest.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+<TestClass()> _
+Public Class CalcularTest
+    <TestMethod()> _
+    Public Sub DiferencaEntreDatasTest()
+        'Dim dataInicial As DateTime = DateTime.Now.Date
+        Dim dataInicial As DateTime = New DateTime(2013, 2, 4)
+        Dim dataFinal As DateTime = New DateTime(2013, 2, 10)
+        Dim retorno = Calcular.QuantidadeDeDiasUteisEntreDatas(dataInicial, dataFinal, 0)
+        Assert.AreEqual(retorno, 5)
+    End Sub
+End Class
---
+++ Roteiro/ImpactaVbNet1/Impacta.Infra.Apoio/Calcular.vb
@@ -0,0 +1,29 @@
+﻿Public Class Calcular
+    ''' <summary>
+    ''' Retorna a quantidade de dias úteis entre duas datas, descontando os feriados.
+    ''' </summary>
+    ''' <param name="dataInicial">Data inicial - as horas serão desconsideradas.</param>
+    ''' <param name="dataFinal">Data final - as horas serão desconsideradas.</param>
+    ''' <param name="quantidadeFeriados"></param>
+    ''' <returns>Um inteiro representando a quantidade de dias.</returns>
+    ''' <remarks></remarks>
+    Public Shared Function QuantidadeDeDiasUteisEntreDatas(dataInicial As DateTime, dataFinal As DateTime, quantidadeFeriados As Int32) As Int32
+        If dataInicial > dataFinal Then
+            Throw New ArgumentOutOfRangeException("A data inicial não pode ser maior que a data final.")
+        End If
+        If quantidadeFeriados < 0 Then
+            Throw New ArgumentOutOfRangeException("A quantidade de feriados não pode ser menor que zero.")
+        End If
+
+        Dim quantidadeDeDiasUteis = 0
+
+        While dataInicial.Date <= dataFinal.Date
+            If dataInicial.DayOfWeek <> DayOfWeek.Saturday AndAlso dataInicial.DayOfWeek <> DayOfWeek.Sunday Then
+                quantidadeDeDiasUteis += 1
+            End If
+            dataInicial = dataInicial.AddDays(1)
+        End While
+
+        Return quantidadeDeDiasUteis - quantidadeFeriados
+    End Function
+End Class
