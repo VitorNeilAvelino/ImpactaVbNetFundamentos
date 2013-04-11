9855a2eb1d465044ae8e4aebc3b8cbf2f0f510a4
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-04-11 19:53:34 +0000

    Teste para manipulação de XML.

---
+++ Roteiro/ImpactaVbNet1/Impacta.Infra.Repositorios.Xml.Testes/XmlRepositorioTest.vb
@@ -0,0 +1,10 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+<TestClass()> _
+Public Class XmlRepositorioTest
+    <TestMethod()> _
+    Public Sub ExtrairValordoNohTest()
+        Dim repositorio As XmlRepositorio = New XmlRepositorio()
+        Dim retorno = repositorio.ExtrairValorDoNoh("C:\Users\vavelino\Desktop\1.xml", "http://www.portalfiscal.inf.br/nfe", "vICMS")
+        Assert.AreEqual(retorno, "3436.07")
+    End Sub
+End Class
---
+++ Roteiro/ImpactaVbNet1/Impacta.Infra.Repositorios.Xml/XmlRepositorio.vb
@@ -0,0 +1,10 @@
+﻿Public Class XmlRepositorio
+
+    Public Function ExtrairValorDoNoh(caminhoArquivoXml As String, xmlNameSpace As XNamespace, nomeNoh As String) As String
+        Dim arquivoXml = XDocument.Load(caminhoArquivoXml)
+        Dim nos = arquivoXml.Descendants(xmlNameSpace + nomeNoh)
+        Dim retorno = nos.FirstOrDefault()
+        Return If(retorno Is Nothing, Nothing, retorno.Value)
+    End Function
+
+End Class
