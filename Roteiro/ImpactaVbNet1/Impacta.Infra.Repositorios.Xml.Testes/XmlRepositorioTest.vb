74e6498713f5191dcb5fa2881f75d53810611d0d
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-04-12 13:43:28 +0000

    Teste com FirstOrDefault.

--- Roteiro/ImpactaVbNet1/Impacta.Infra.Repositorios.Xml.Testes/XmlRepositorioTest.vb
+++ Roteiro/ImpactaVbNet1/Impacta.Infra.Repositorios.Xml.Testes/XmlRepositorioTest.vb
@@ -1,10 +1,18 @@
 ﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
 <TestClass()> _
 Public Class XmlRepositorioTest
+
     <TestMethod()> _
-    Public Sub ExtrairValordoNohTest()
+    Public Sub ExtrairValorDoNohViaResource()
         Dim repositorio As XmlRepositorio = New XmlRepositorio()
-        Dim retorno = repositorio.ExtrairValorDoNoh("C:\Users\vavelino\Desktop\1.xml", "http://www.portalfiscal.inf.br/nfe", "vICMS")
+        Dim retorno = repositorio.ExtrairValorDoNoh(XDocument.Parse(My.Resources.XmlParaTesteComDescendants), "http://www.portalfiscal.inf.br/nfe", "vICMS")
         Assert.AreEqual(retorno, "3436.07")
     End Sub
+
+    Public Sub ExtrairValorDoNohViaResourceComParametroIncorreto()
+        Dim repositorio As XmlRepositorio = New XmlRepositorio()
+        Dim retorno = repositorio.ExtrairValorDoNoh(XDocument.Parse(My.Resources.XmlParaTesteComDescendants), "http://www.portalfiscal.inf.br/nfe", "issoNonEcziste")
+        Assert.AreEqual(retorno, Nothing)
+    End Sub
+    
 End Class
--- Roteiro/ImpactaVbNet1/Impacta.Infra.Repositorios.Xml/XmlRepositorio.vb
+++ Roteiro/ImpactaVbNet1/Impacta.Infra.Repositorios.Xml/XmlRepositorio.vb
@@ -1,8 +1,11 @@
 ﻿Public Class XmlRepositorio
 
     Public Function ExtrairValorDoNoh(caminhoArquivoXml As String, xmlNameSpace As XNamespace, nomeNoh As String) As String
-        Dim arquivoXml = XDocument.Load(caminhoArquivoXml)
-        Dim nos = arquivoXml.Descendants(xmlNameSpace + nomeNoh)
+        Return ExtrairValorDoNoh(XDocument.Load(caminhoArquivoXml), xmlNameSpace, nomeNoh)
+    End Function
+
+    Public Function ExtrairValorDoNoh(xDocument As XDocument, xmlNameSpace As XNamespace, nomeNoh As String) As String
+        Dim nos = xDocument.Descendants(xmlNameSpace + nomeNoh)
         Dim retorno = nos.FirstOrDefault()
         Return If(retorno Is Nothing, Nothing, retorno.Value)
     End Function
