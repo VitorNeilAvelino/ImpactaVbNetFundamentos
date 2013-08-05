1f36278f2de3162382ea68895e4ad2436c23a32c
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-08-05 00:15:25 +0000

    CrudForm

---
+++ PlantaoDeDuvidas/VbNet1.PlantaoDeDuvidas.WindowsForms/CrudForm.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+
+Public Class CrudForm
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim conexao As New SqlConnection("Data Source=.\sqlexpress;Initial Catalog=ImpactaVbNet1;Integrated Security=True;Pooling=False")
+        Dim instrucao = String.Format("Insert into Cliente(Nome, DataNascimento, Email, Endereco) values('{0}', '{1}', '{2}', '{3}')", TextBox1.Text, DateTime.Now, "Email",
+                                      "Endereço")
+        Dim comando As New SqlCommand(instrucao, conexao)
+        conexao.Open()
+        comando.ExecuteNonQuery()
+        conexao.Close()
+    End Sub
+End Class
