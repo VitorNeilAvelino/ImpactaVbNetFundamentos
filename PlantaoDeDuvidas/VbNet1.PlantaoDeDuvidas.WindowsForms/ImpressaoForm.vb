8a43e08bd5c29eaddd6ebdd04799bf5f4506216c
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2013-08-27 14:59:26 +0000

    Impressão.

---
+++ PlantaoDeDuvidas/VbNet1.PlantaoDeDuvidas.WindowsForms/ImpressaoForm.vb
@@ -0,0 +1,16 @@
+﻿Public Class ImpressaoForm
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim myText As String = "Vertical text"
+
+        Dim fontFamily As New FontFamily("Lucida Console")
+        Dim font As New Font(fontFamily, 14, FontStyle.Regular, GraphicsUnit.Point)
+        Dim stringFormat As New StringFormat()
+        Dim solidBrush As New SolidBrush(Color.FromArgb(255, 0, 0, 255))
+
+        stringFormat.FormatFlags = StringFormatFlags.DirectionVertical
+
+        'e.Graphics.DrawString(myText, font, solidBrush, 50, 50, stringFormat)
+
+    End Sub
+End Class
