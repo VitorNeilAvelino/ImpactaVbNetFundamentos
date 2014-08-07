ced5a16bd74a2e6893b2e07d9018c6c8e3a96011
vitoravelino_cp <13843364+VitorNeilAvelino@users.noreply.github.com>
2014-08-07 23:31:03 +0000

    Repositorios.MsAccess

---
+++ Roteiro/ImpactaVbNet1/VbNet1.Repositorios.MsAccess.Testes/ClienteRepositorioTeste.vb
@@ -0,0 +1,13 @@
+﻿<TestClass()>
+Public Class ClienteRepositorioTeste
+    <TestMethod()>
+    Public Sub InserirTest()
+        Dim cliente As New Impacta.Dominio.Cliente()
+        cliente.Nome = "Vítor"
+        cliente.DataNascimento = Convert.ToDateTime("25/12/1970")
+        cliente.Email = "avelino.vitor@gmail.com"
+
+        Dim repositorio = New ClienteRepositorio()
+        repositorio.Inserir(cliente)
+    End Sub
+End Class
---
+++ Roteiro/ImpactaVbNet1/VbNet1.Repositorios.MsAccess/ClienteRepositorio.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.OleDb
+
+Public Class ClienteRepositorio
+    Public Sub Inserir(cliente As Impacta.Dominio.Cliente)
+
+        Dim caminho = "C:\Users\Vitor\Documents\Aulas\Impacta\CSharp\II\Pedido.accdb"
+
+        Using con As New OleDbConnection(String.Format("Provider=Microsoft.ACE.OLEDB.12.0;DataSource={0};", caminho))
+
+            con.Open()
+
+            Using cmd As New OleDbCommand
+                cmd.CommandText = "Insert Cliente (Nome, DataNascimento, Email) values(?, ?, ?)"
+
+                cmd.Parameters.AddWithValue("@p1", cliente.Nome)
+                cmd.Parameters.AddWithValue("@p2", cliente.DataNascimento)
+                cmd.Parameters.AddWithValue("@p3", cliente.Email)
+
+                cmd.ExecuteNonQuery()
+            End Using
+        End Using
+    End Sub
+End Class
